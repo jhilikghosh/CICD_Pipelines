144e397505e949e43d87302dc32e80ff1f9358c3
Shasank <110684323+Yeswanth-Kaki@users.noreply.github.com>
2023-04-04 15:40:26 +0100

    Adding the base code for jenkins Integration
    Add the

---
+++ src/main/java/com/example/SampleProjectForJenkinsIntegration/SampleProjectForJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.SampleProjectForJenkinsIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectForJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectForJenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SampleProjectForJenkinsIntegration/SampleProjectForJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SampleProjectForJenkinsIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProjectForJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
